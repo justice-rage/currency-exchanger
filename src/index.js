be076e5db9b715fbd7ff7be46dac98ffd78c0cf1
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-26 11:26:51 -0700

    add getElements block

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import CurrencyConverter from './js/exchange-rate';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyConverter from './js/exchange-rate';
 
 function clearFields() {
   $('#usd').text("");
   $('.showErrors').text("");
   $('.showConversionRate').text("");
+}
+
+function getElements(response) {
+  if (response) {
+    const selectedCurrency = (response.target_code);
+    const usdConverted = (response.conversion_result);
+    $('.showConversionRate').append(`Your USD amount equates to ${usdConverted} ${selectedCurrency}`);
+  } else {
+    $('.showErrors').text(`Error: ${response}`);
+  }
 }
 
 $(document).ready(function() {
