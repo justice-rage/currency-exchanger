196a7aaf93d6218111fc1a025eb10368913fa004
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-25 19:33:34 -0700

    add API call management logic

--- src/js/exchange-rate.js
+++ src/js/exchange-rate.js
@@ -2,6 +2,12 @@ export default class CurrencyConverter {
   static async exchangeRate(selectedCurrency, usdAmount) {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${selectedCurrency}/${usdAmount}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch (error) {
+      return error.message;
     }
   }
 }
