200929115473b5de51f600f5c12ed5542650d4bb
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-26 11:22:55 -0700

    create API_KEY variable

--- src/js/exchange-rate.js
+++ src/js/exchange-rate.js
@@ -1,7 +1,8 @@
 export default class CurrencyConverter {
   static async exchangeRate(selectedCurrency, usdAmount) {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${selectedCurrency}/${usdAmount}`);
+      const API_KEY = process.env.API_KEY;
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/pair/USD/${selectedCurrency}/${usdAmount}`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
