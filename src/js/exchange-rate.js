7e486e30b75199fddfe7cbbc28c9e67775e8a826
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-03-25 19:30:37 -0700

    add API request

--- src/js/exchange-rate.js
+++ src/js/exchange-rate.js
@@ -1,3 +1,7 @@
 export default class CurrencyConverter {
-  
+  static async exchangeRate(selectedCurrency, usdAmount) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${selectedCurrency}/${usdAmount}`);
+    }
+  }
 }
